2928d649e667b806f2f43ed6de10b926d3bdaf94
Tendy <a4100791ex@gmail.com>
2026-05-11 19:09:42 +0800

    Trim code

--- backend/cd-forceEject.sh
+++ backend/cd-forceEject.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-
-# 1. 停止播放，釋放光碟機佔用
 mpc stop
 
-# 2. 執行 coissac / plybrd 的退片清理邏輯 (清除 UI 上的 CD 資訊)
 /usr/local/bin/moodecdplayer --on-eject
 
-# 3. 實體退片 (若你的光碟機是 cdrom 請自行更改)
 eject /dev/sr0
--- ui/php/EjectCD.php
+++ ui/php/EjectCD.php
@@ -1,7 +1,6 @@
 <?php
 header('Content-Type: application/json');
 
-// 呼叫我們剛剛建立的「一次性退片腳本」
 $output = shell_exec('sudo /usr/local/bin/cd-forceEject.sh 2>&1');
 
 echo json_encode([
